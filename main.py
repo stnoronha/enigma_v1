dc4dd7b5cb253e8095ec0d044d88574301eb93f0
stnoronha <sharont.noronha@gmail.com>
2024-02-18 22:33:22 -0500

    feat:copied sample code from enigma documentation

--- main.py
+++ main.py
@@ -4,13 +4,35 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+from enigmatoolbox.datasets import load_summary_stats
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+
+
+# Get case-control cortical thickness and surface area tables
+
+#CT = sum_stats['CortThick_case_vs_controls']
+#SA = sum_stats['CortSurf_case_vs_controls']
+
+# Extract Cohen's d values
+
+#CT_d = CT['d_icv']
+#SA_d = SA['d_icv']
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    sum_stats = load_summary_stats('22q')
+    CT = sum_stats['CortThick_case_vs_controls']
+    SA = sum_stats['CortSurf_case_vs_controls']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Extract Cohen's d values
+
+    CT_d = CT['d_icv']
+    SA_d = SA['d_icv']
+
+    print(CT_d)
