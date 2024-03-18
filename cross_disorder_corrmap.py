e8b33faaf4923e1567ed3a2d68c89bd1a0d3a3e6
stnoronha <sharont.noronha@gmail.com>
2024-03-17 20:50:46 -0400

    add plotting between default and z-scored components

--- cross_disorder_corrmap.py
+++ cross_disorder_corrmap.py
@@ -1,13 +1,23 @@
 from enigmatoolbox.cross_disorder import cross_disorder_effect
 from numpy import corrcoef
 from seaborn import heatmap
+import matplotlib.pyplot as plt
+
+from cross_disorder_copy import cross_disorder_effect_z
+import enigmatoolbox.cross_disorder
 
 # Extract shared disorder effects
+components, variance, names = enigmatoolbox.cross_disorder.cross_disorder_effect()
+components_z, variance_z, names_z = cross_disorder_effect_z()
 
-correlation_matrix, names = cross_disorder_effect(method='correlation')
+print(variance['cortex'][:5])
 
-# Plot correlation matrices
+# Plot correlation matrices between zscored and default PCA output components (10 largest components
+heatmap(corrcoef(components['cortex'][:10], components_z['cortex'][:10]))
 
+plt.title('Correlation matrix of top 10 PCA components in Z-scored vs default data', fontsize = 20)
+plt.xlabel('Default', fontsize = 15)
+plt.ylabel('Z-scored', fontsize = 15)
+plt.show()
 
-
-heatmap(corrcoef(components['cortex'], components_z['cortex']), annot=True)
+# Find loading matrix by multiplying each component by the sqare root of its corresponding eigenvalue
