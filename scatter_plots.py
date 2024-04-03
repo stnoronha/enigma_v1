9cc96a42e03f1dbe4c1a2200c44449f613a0930c
stnoronha <sharont.noronha@gmail.com>
2024-04-03 18:15:12 -0400

    uncomment scatter plots for UMAP/PCA, remove print statements and unused packages

--- scatter_plots.py
+++ scatter_plots.py
@@ -1,18 +1,12 @@
 from cross_disorder_copy import cross_disorder_effect_z
 import umap.plot
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.decomposition import PCA
-from numpy import linalg as LA
-import pandas as pd
-from scipy import stats
+
 
 # UMAP
-
 umap_cor, umap_sub, umap_comp, names = cross_disorder_effect_z(method="umap")
 
-'''
+
 # Cortex UMAP plot
 corplot = umap.plot.points(umap_cor)
 corplot.set_title('Cortex')
@@ -23,10 +17,10 @@ umap.plot.points(umap_sub)
 subplot = umap.plot.points(umap_sub)
 subplot.set_title('Subcortex')
 #plt.show()
-'''
+
 
 components, variance, names = cross_disorder_effect_z(method='pca')
-'''
+
 plt.scatter(components['cortex'][:,0], components['cortex'][:,1])
 plt.title('Cortex Component 1 v Component 2')
 plt.xlabel('PC1')
@@ -38,10 +32,8 @@ plt.title('Subcortex Component 1 v Component 2')
 plt.xlabel('PC1')
 plt.ylabel('PC2')
 #plt.show()
-'''
+
 # PCA vs UMAP components
-print(np.shape(components['cortex'][:50,0]))
-print(np.shape(umap_comp['cortex'][:,0]))
 plt.scatter(components['cortex'][:50,0], umap_comp['cortex'][:,0])
 plt.title('PCA Components vs UMAP components')
 plt.xlabel('PCA')
