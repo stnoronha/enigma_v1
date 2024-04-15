583230df405e9310953e0c55dd8e5d7dc8039d3f
stnoronha <sharont.noronha@gmail.com>
2024-04-15 10:44:12 -0400

    Removed disorder specific brain plotting code

--- brain_plots.py
+++ brain_plots.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 from enigmatoolbox.utils import parcel_to_surface
 from cross_disorder_copy import cross_disorder_effect_z
 import numpy as np
+from enigmatoolbox.datasets.base import load_summary_stats
 
 # Visualize the first cortical and subcortical components on the surface brains
 
 components_z, variance_z, names_z = cross_disorder_effect_z(measure='CortThick')
 umap_cor, umap_sub, umap_comp, names = cross_disorder_effect_z(measure='CortThick', method="umap")
 
-print(umap_comp['cortex'].shape)
-
-print(components_z['cortex'].shape)
-
+'''
 # Z scored PCA brain maps
 plot_cortical(parcel_to_surface(components_z['cortex'][:, 0], 'aparc_fsa5'), color_range=(-0.5, 0.5),
 
@@ -38,3 +36,10 @@ plot_subcortical(umap_comp['subcortex'][:, 0], color_range=(-0.5, 0.5),
                  cmap='RdBu_r', color_bar=True, size=(800, 400))
 
 plt.show()
+'''
+CT_d = load_summary_stats()
+
+CT_d_fsa5 = parcel_to_surface(CT_d, 'aparc_fsa5')
+pca_fsa5 = parcel_to_surface(CT_d_pca[:,0], 'aparc_fsa5')
+plot_cortical(array_name=pca_fsa5, surface_name="fsa5", size=(800, 200),
+              cmap='RdBu_r', color_bar=True)
