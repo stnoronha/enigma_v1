f29793915456ecdc0f6bdd1b54edcd31af48a5c6
stnoronha <sharont.noronha@gmail.com>
2024-04-15 10:48:16 -0400

    uncommented plt.show()

--- scatter_plots.py
+++ scatter_plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 # UMAP
-umap_cor, umap_sub, umap_comp, names = cross_disorder_effect_z(measure='CortThick',method="umap")
+umap_cor, umap_sub, umap_comp, names = cross_disorder_effect_z(method="umap")
 
 
 # Cortex UMAP plot
@@ -17,23 +17,25 @@ subplot = umap.plot.points(umap_sub)
 subplot.set_title('Subcortex')
 plt.show()
 
-components, variance, names = cross_disorder_effect_z(measure = 'CortThick',method='pca')
+components, variance, names = cross_disorder_effect_z(method='pca')
 
-plt.scatter(components['cortex'][:,0], components['cortex'][:,1])
+
+
+plt.scatter(components['cortex'][:,0], components['cortex'][:,1],color="#ff7f0e")
 plt.title('Cortex Component 1 v Component 2')
 plt.xlabel('PC1')
 plt.ylabel('PC2')
-#plt.show()
+plt.show()
 
 plt.scatter(components['subcortex'][:,0], components['subcortex'][:,1])
 plt.title('Subcortex Component 1 v Component 2')
 plt.xlabel('PC1')
 plt.ylabel('PC2')
-#plt.show()
+plt.show()
 
 # PCA first component vs UMAP first component
-plt.scatter(components['cortex'][:,0], umap_comp['cortex'][:,0])
-plt.title('PCA Components vs UMAP components')
+plt.scatter(components['cortex'][:,1], umap_comp['cortex'][:,1])
+plt.title('PCA 2nd Components vs UMAP 2nd component')
 plt.xlabel('PCA')
 plt.ylabel('UMAP')
 plt.show()
