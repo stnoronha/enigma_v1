2e480a0a0098118be9d608ddad2166377f973265
stnoronha <sharont.noronha@gmail.com>
2024-04-19 10:43:42 -0400

    change color ranges on UMAP related brain maps so that components are visible

--- brain_plots.py
+++ brain_plots.py
@@ -14,7 +14,7 @@ umap_cor, umap_sub, umap_comp, names = cross_disorder_effect_z(method="umap")
 
 
 # Z scored PCA brain maps
-plot_cortical(parcel_to_surface(components_z['cortex'][:, 0], 'aparc_fsa5'), color_range=(-0.5, 0.5),
+plot_cortical(parcel_to_surface(components_z['cortex'][:, 0], 'aparc_fsa5'), color_range=(-1, 2),
 
               cmap='RdBu_r', color_bar=True, size=(800, 400))
 
@@ -25,18 +25,18 @@ plot_subcortical(components_z['subcortex'][:, 0], color_range=(-0.5, 0.5),
 
                  cmap='RdBu_r', color_bar=True, size=(800, 400))
 
-#plt.show()
+plt.show()
 
 
 # Z scored UMAP brain maps
-plot_cortical(parcel_to_surface(umap_comp['cortex'][:, 0], 'aparc_fsa5'), surface_name='fsa5', color_range=(-0.5, 0.5),
+plot_cortical(parcel_to_surface(umap_comp['cortex'][:, 0], 'aparc_fsa5'), surface_name='fsa5', color_range=(-1, 2),
 
               cmap='RdBu_r', color_bar=True, size=(800, 400))
 
 plt.show()
 
 
-plot_subcortical(umap_comp['subcortex'][:, 0], color_range=(-0.5, 0.5),
+plot_subcortical(umap_comp['subcortex'][:, 0], color_range=(1, 4),
 
                  cmap='RdBu_r', color_bar=True, size=(800, 400))
 
