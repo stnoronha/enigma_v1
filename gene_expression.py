e8627b2503f25b30d851c00828c1279c74535133
stnoronha <sharont.noronha@gmail.com>
2024-04-25 09:23:13 -0400

    added code to retrieve significant gene expression and record genes in text file

--- gene_expression.py
+++ gene_expression.py
@@ -7,6 +7,7 @@ import numpy as np
 from enigmatoolbox.datasets.base import load_summary_stats
 from enigmatoolbox.utils.parcellation import parcel_to_surface, surface_to_parcel
 from scipy.stats import spearmanr
+import csv
 
 # PCA cross disorder effect
 components_z, variance_z, names_z = cross_disorder_effect_z()
@@ -21,6 +22,14 @@ reglabels = genes['label']
 genes = genes.drop('label', axis=1)
 genelabels = list(genes.columns)
 
+'''
+with open('gene_labels.txt', 'w') as f:
+    for line in genelabels:
+        f.write("%s\n" % line)
+
+print(genelabels[:10])
+'''
+
 def corr_gene_exp(solution):
     if len(solution) == 200:
         # put it on a surface
@@ -33,8 +42,8 @@ def corr_gene_exp(solution):
         d_mri = solution
     else:
         raise ValueError('Solution must be 200 or 68 long')
-    r_gene = []
-    p_gene = []
+    r_gene = [] #R is correlation coefficient
+    p_gene = [] #p is probability that correlation exists if null is true
     for i, gene in enumerate(genelabels):
         geneexp = genes.iloc[0:68,i]
         # get the location of non-nan values
@@ -48,3 +57,37 @@ r_gene_pca, p_gene_pca = corr_gene_exp(components_z['cortex'][:,0])
 r_gene_umap, p_gene_umap = corr_gene_exp(umap_comp['cortex'][:,0])
 
 # Create dictionary with gene names and correlations
+gene_pca_list = [
+    {'name':genelabels,'r_gene':r_gene_pca,'p_gene':p_gene_pca}
+    for genelabels,r_gene_pca,p_gene_pca in zip(genelabels,r_gene_pca,p_gene_pca)]
+
+gene_umap_list = [
+    {'name':genelabels,'r_gene':r_gene_umap,'p_gene':p_gene_umap}
+    for genelabels,r_gene_umap,p_gene_umap in zip(genelabels,r_gene_umap,p_gene_umap)]
+
+# pull out all genes which have meet r and p value thresholds
+def max_cor_genes(gene_list, r_thresh, p_thresh):
+    if r_thresh >= 0:
+        return [gene_list[i]['name'] for i in range(len(gene_list)) 
+               if gene_list[i]['r_gene'] >= r_thresh and gene_list[i]['p_gene'] < p_thresh]
+    if r_thresh < 0:
+        return [gene_list[i]['name'] for i in range(len(gene_list)) 
+               if gene_list[i]['r_gene'] <= r_thresh and gene_list[i]['p_gene'] < p_thresh]
+
+
+gene_pca_pos = max_cor_genes(gene_pca_list,0.5,0.05)
+gene_pca_neg = max_cor_genes(gene_pca_list,-0.5,0.05)
+
+gene_umap_pos = max_cor_genes(gene_umap_list,0.5,0.05)
+gene_umap_neg = max_cor_genes(gene_umap_list,-0.5,0.05)
+
+# Write list into a text file
+
+def store_list(gene_list, name):
+    with open(name + str('.txt'), 'w') as f:
+        for line in gene_pca_pos:
+            f.write("%s\n" % line)
+
+neg_combined = list(set(gene_umap_neg).intersection(gene_pca_neg))
+
+store_list(neg_combined,"significant negative")
