54f18af0df5c79db57f3e3f165317e03f42b30af
stnoronha <sharont.noronha@gmail.com>
2024-09-30 17:27:05 -0400

    clean up of unecessary code, particularly subcortical plotting

--- brain_plots.py
+++ brain_plots.py
@@ -9,56 +9,26 @@ from enigmatoolbox.utils.parcellation import parcel_to_surface
 
 # Visualize the first cortical and subcortical components on the surface brains
 
-components_z, variance_z, names_z = cross_disorder_effect_z()
-umap_cor, umap_sub, umap_comp, names = cross_disorder_effect_z(method="umap")
-iso_cor, iso_sub, iso_comp, names = cross_disorder_effect_z(method="isomap")
+components_z, variance_z, names_z = cross_disorder_effect_z(measure="CortThick")
+umap_cor, umap_sub, umap_comp, names = cross_disorder_effect_z(measure="CortThick",method="umap")
 
-print(min(iso_comp['cortex'][:,0]))
-print(max(iso_comp['cortex'][:,0]))
+print(names)
+print(components_z['cortex'][:, 0])
 
-print(min(iso_comp['subcortex'][:,0]))
-print(max(iso_comp['subcortex'][:,0]))
-
-# Z scored PCA brain maps
+# Z scored PCA brain maps for z-scored first component
 plot_cortical(parcel_to_surface(components_z['cortex'][:, 0], 'aparc_fsa5'), color_range=(-5, 6),
-
               cmap='RdBu_r', color_bar=True, size=(800, 400))
-
-plt.show()
-
-
-plot_subcortical(components_z['subcortex'][:, 0], color_range=(-5, 7),
-
-                 cmap='RdBu_r', color_bar=True, size=(800, 400))
 
 plt.show()
 
 
 # Z scored UMAP brain maps
-plot_cortical(parcel_to_surface(umap_comp['cortex'][:, 0], 'aparc_fsa5'), surface_name='fsa5', color_range=(-1, 2),
-
+plot_cortical(parcel_to_surface(umap_comp['cortex'][:, 0], 'aparc_fsa5'), surface_name='fsa5', color_range=(-2, 8),
               cmap='RdBu_r', color_bar=True, size=(800, 400))
 
 plt.show()
 
 
-plot_subcortical(umap_comp['subcortex'][:, 0], color_range=(1, 4),
-
-                 cmap='RdBu_r', color_bar=True, size=(800, 400))
-
-plt.show()
-
-# Z scored isomap brain maps (component ranges are much more varied)
-plot_cortical(parcel_to_surface(iso_comp['cortex'][:, 0], 'aparc_fsa5'), surface_name='fsa5', color_range=(-14, 14),
-
-              cmap='RdBu_r', color_bar=True, size=(800, 400))
-
-plt.show()
 
 
-plot_subcortical(iso_comp['subcortex'][:, 0], color_range=(-14, 14),
-
-                 cmap='RdBu_r', color_bar=True, size=(800, 400))
-
-plt.show()
 
